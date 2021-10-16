a10b2f5777427e8da6fcc4259c69e54a752949a9
Hacker-cmd-dota <63328484+Hacker-cmd-dota@users.noreply.github.com>
2021-10-16 12:02:16 +0700

    Update dmass.py

--- dmass.py
+++ dmass.py
@@ -44,4 +44,4 @@ async def send(ctx, *, content: str):
                 await client.say("DM can't Sent To : {} :x: ".format(member))
 
 
-client.run("ODk4NzY5OTA1Nzc4ODUxODgw.YWpCsw.4RgmLS7KpTxIm_gWRE79ialLzQI")                
+client.run("Your_Bot_Token")                
